bdc88e08de50d81804227c3a50f721e8edc99c03
Thiago Ferreira <contato@pandoapps.com.br>
2026-04-21 21:29:18 -0300

    deploy

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -19,6 +19,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_FOR
+            | Request::HEADER_X_FORWARDED_HOST
+            | Request::HEADER_X_FORWARDED_PORT
+            | Request::HEADER_X_FORWARDED_PROTO
+            | Request::HEADER_X_FORWARDED_AWS_ELB
+        );
+
         $middleware->alias([
             'role' => EnsureRole::class,
             'producer.approved' => EnsureApprovedProducer::class,
